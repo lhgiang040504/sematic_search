c38c30043505dd4263ad57232fbe254ef1829d94
lhgiang040504 <lhgiang040504@gmail.com>
2025-05-20 12:11:17 +0700

    clean

--- src/api.py
+++ src/api.py
@@ -1,9 +1,9 @@
-from typing import Any
-
-from fastapi import Body, APIRouter, Request
+from typing import Any, Optional
+from fastapi import Body, APIRouter, Request, Query
 
 from src.ingestion.ingest import ingest_pipeline
-from src.model import ConnectorType
+from src.search.search import search_pipeline
+from src.model import ConnectorType, SearchStrategyType, Passage, PassageResponse
 
 api = APIRouter(prefix="/api")
 
@@ -18,8 +18,23 @@ def ingest_endpoint(
             "max_size": 100,
             "chunk_size": 100,
         })) -> dict[str, Any]:
+    
     ingest_pipeline(request=req, connector_type=connector_type, config=config)
     return {
         "message": "Documents ingested successfully"
     }
 
+
+@api.get("/search",)
+async def search_endpoint(
+        req: Request, 
+        strategy_type: SearchStrategyType = SearchStrategyType.HYBRID_SEARCH,
+        query: str = Query(default="does human hair stop squirrels", description="MS MARCO Dataset: https://huggingface.co/datasets/microsoft/ms_marco/viewer/v1.1/train"),
+        max_results: int = 5,
+        ) -> dict[str, Any]:
+        
+    search_results = search_pipeline(req, strategy_type, query, max_results)
+    return {
+        "results": search_results,
+        "total_results": len(search_results),
+    }
--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
+from typing import Any
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from dotenv import dotenv_values
-config = dotenv_values(".env")
 
 from src.api import api
 from src.utils.db_connection.mongodb_connector import MongoDB
 
 
-from typing import Any
+# Load environment variables
+from dotenv import dotenv_values
+config = dotenv_values(".env")
 
 app = FastAPI(
     title="Semantic Search",
@@ -23,7 +24,8 @@ app.add_middleware(
     allow_credentials=True,
 )
 
-# INITIALIZE MONGODB CONNECTION
+
+# MongoDB connection
 @app.on_event("startup")
 def startup_db_client():
     app.mongodb = MongoDB(uri=config['MONGODB_URI'], db_name=config['MONGODB_DB'])
@@ -31,17 +33,22 @@ def startup_db_client():
     print("Connected to MongoDB database!")
 
     # Test
-    db = app.mongodb.get_database()
-    collection = db["documents"]  # or your collection name
-    collection.insert_one({'id': 'abc', 'passage': 'abc'})
-    
+    # db = app.mongodb.get_database()
+    # collection = db["documents"]  # or your collection name
+    #collection.insert_one({'passage_id': 'abc', 'content': 'abc', 'doc_id': 'abc', 'embedding': [1.0, 2.0, 3.0]})
+    #print("Inserted test document into MongoDB collection!")
+
+
+# Close the MongoDB connection on shutdown
 @app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb.close()  # Close the MongoDB connection
     print("Disconnected from MongoDB database!")
 
+
 # Routers
 app.include_router(api)
+
 
 # Index page
 @app.get("/")
