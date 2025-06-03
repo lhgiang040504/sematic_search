b27796cdd2bf185a1ab8ebec7a64c4e9dd84869d
lhgiang040504 <lhgiang040504@gmail.com>
2025-06-03 13:50:22 +0700

    another dataset for comparing

--- src/ingestion/connector/connector_main.py
+++ src/ingestion/connector/connector_main.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from src.model import ConnectorType
 from src.ingestion.connector.huggingface_dataset_connector import HuggingFaceConnector
+from src.ingestion.connector.Cranfield_dataset_connector import Cranfield
 
 def get_connector_map(config: dict[str, Any]):
     return {
@@ -11,5 +12,8 @@ def get_connector_map(config: dict[str, Any]):
             split=config["split"],
             max_size=config["max_size"],
             chunk_size=config["chunk_size"]
-        )
+        ),
+        ConnectorType.CRANFIELD: lambda: Cranfield(
+            dataset_folderpath=config["dataset_folderpath"],
+        ),
     }
