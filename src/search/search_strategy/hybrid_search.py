4afa18c8a36a40c58072b24e344013ecec8a2ddf
lhgiang040504 <lhgiang040504@gmail.com>
2025-06-08 12:16:26 +0700

    no need to use query expand

--- src/search/search_strategy/hybrid_search.py
+++ src/search/search_strategy/hybrid_search.py
@@ -7,9 +7,19 @@ from src.search.search_strategy.abstract_search_strategy import SearchStrategy, 
 
 class HybridSearch(SearchStrategy):
     def search(self, request: Request, config: dict[str, Any]) -> List[Passage]:
-        expanded_query = expand_query(config['query'])
-        print(f"Expanded Query: {expanded_query}")
+        #config['query'] = expand_query(config['query'])
+        print(f"Expanded Query: {config['query']}")
+
+        # is_simple = classify_query(config['query'])
+        # if is_simple:
+        #     passages = keyword_search(request, config)
+        # else:
+        #     passages = vector_search(request, config)
+        
         passages = vector_search(request, config)
+        if not passages or len(passages) == 0:
+            print("No passages found with vector search, falling back to keyword search.")
+            return keyword_search(request, config)
         print(f"Vector Search Results: {len(passages)} passages found")
         return passages
 
