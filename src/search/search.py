bc1a739bcc94852e6c1f48e106d5b2785ddb518b
lhgiang040504 <lhgiang040504@gmail.com>
2025-06-16 15:06:42 +0700

    remove request

--- src/search/search.py
+++ src/search/search.py
@@ -1,18 +1,19 @@
 from typing import Optional, Any
+from prefect import flow
 
 from src.model import SearchStrategyType
 from src.search.search_strategy.search_strategy_factory import get_search_strategy_map
 from fastapi import Request
 
-
-def search_pipeline(request: Request, strategy_type: SearchStrategyType, config: dict[str, Any]):
+@flow(name="Search Pipeline")
+def search_pipeline(strategy_type: SearchStrategyType, config: dict[str, Any]):
     # Get the search strategy based on the strategy type
     search_strategy = get_search_strategy_map().get(strategy_type)
     if not search_strategy:
         raise ValueError(f"Unknown search strategy type: {strategy_type}")
 
     # Perform the search using the selected strategy
-    search_results = search_strategy.search(request, config)
+    search_results = search_strategy.search(config)
 
     return search_results
 
