0736e19193020f7f17372e0e7d9f5d8cd789a403
lhgiang040504 <lhgiang040504@gmail.com>
2025-06-16 15:07:18 +0700

    add prefecct

--- src/ingestion/ingest.py
+++ src/ingestion/ingest.py
@@ -1,5 +1,7 @@
 from typing import Any, List
 from fastapi import Request
+from prefect import flow
+from prefect import task
 
 from src.utils.hash import generate_md5_hash
 from src.ingestion.chunking import semantic_chunk
@@ -7,6 +9,8 @@ from src.ingestion.connector.connector_main import get_connector_map
 from src.model import Passage, ConnectorType
 from src.ingestion.embedding import generate_embedding 
 
+
+@flow(name="Ingest Pipeline")
 def ingest_pipeline(request: Request, connector_type: ConnectorType, config: dict[str, Any]):
     # Connect to data source
     connector_function = get_connector_map(config).get(connector_type)
@@ -37,9 +41,11 @@ def ingest_pipeline(request: Request, connector_type: ConnectorType, config: dic
             )
             index_document(doc_to_store, collection=collection)
 
+@task
 def clean_document(content: str) -> str:
     return content
 
+@task
 def index_document(passage: Passage, collection) -> None:
     collection.update_one(
         {"passage_id": passage.passage_id},
@@ -47,5 +53,6 @@ def index_document(passage: Passage, collection) -> None:
         upsert=True
     )
 
+@task
 def generate_passage_embedding(content: str) -> List[float]:
     return generate_embedding(content)
